1569a703d90cbff1099ab163c4fb9b739fc36ff2
David Obodo <obododavid5@gmail.com>
2022-04-04 15:04:26 +0100

    ✨FEATURE: Set "Normals shader" on texture

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -9,4 +9,5 @@ float PI = 3.1415926553589793238;
 
 void main(){
     gl_FragColor = vec4(1., 0., 0., 1.);
+    gl_FragColor = vec4(vNormal, 1.);
 }
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -2,9 +2,11 @@ uniform float time;
 varying vec2 vUv;
 varying vec3 vPosition;
 varying vec2 pixels;
+varying vec3 vNormal;
 float PI = 3.1415926553589793238;
 
 void main(){
     vUv = uv;
+    vNormal = normal;
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
